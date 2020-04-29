6dff9a7994b0c5e0869a6e919a71c9d57da60181
John <jbbennet@ucsc.edu>
2020-04-29 10:08:02 -0700

    added candy wrapper

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 
 
-window.onload=function(){
+
 var items = document.getElementsByClassName('fill');
+var boxes = document.getElementsByClassName('box');
+var boxes= document.getElementsByClassName('box');
 var SSB = document.getElementById('SSB');
 var modal=document.getElementById('simpleModal'); 
 var modalHeader=document.getElementById('mHcontent');
-var closeBtn = document.getElementById('closeBtn');
 var recBtn = document.getElementById('recyclable');
 var nrecBtn = document.getElementById('notRecyclable');
+var okBtn = document.getElementsByClassName('okay');
 var currElement=document.getElementById('SSB');
 var pause=false;
 var answer;
 var audio=new Audio('FMB.mp3');
-var poop;
 
 
 
-const picNames=["images/plastBottle.jpg","images/Coffee.jpg","images/Coors.jpg"];
+
+//const picNames=["images/plastBottle.jpg","images/Coffee.jpg","images/Coors.jpg"];
 var foo;
 
 
@@ -24,14 +26,14 @@ var foo;
 for(var i=0; i<items.length; i++)
 {
 	var img = new Image();
-	img.src=picNames[i];
+	//img.src=picNames[i];
 	items[i].addEventListener('dragstart', dragStart);
 	items[i].addEventListener('dragend', dragEnd);
 	items[i].active=false;
-	items[i].style.backgroundImage=`url('${img.src}')`;
-	items[i].style.height=`${img.height}px`;
-	items[i].style.width= `${img.width}px`;
-	items[i].style.top =`${i*100}px`;
+	//items[i].style.backgroundImage=`url('${img.src}')`;
+	//items[i].style.height=`${img.height}px`;
+	//items[i].style.width= `${img.width}px`;
+	boxes[i].style.top =`${i*320}px`;
 
 }
 
@@ -46,25 +48,22 @@ for(var i=0; i<items.length; i++)
   nrecBtn.addEventListener('click',closeModal);
 
 
-moveTrash();
+moveBoxes();
 
-function moveTrash()
+function moveBoxes()
 {
 	if(!pause)
 	{
-		for(var i=0; i<items.length; i++)
+		for(var i=0; i<boxes.length; i++)
 		{
-			var c = items[i].style.top;
+			var c = boxes[i].style.top;
 			n= parseInt(c);
-			//console.log(fill.style.left);
-			items[i].style.top=`${n-1}px`;
-			//console.log(fill.style.left);
-
-			if(n<0)
-				items[i].style.top="500px";
+			boxes[i].style.top=`${n-1}px`;
+			if(n<-320)
+				boxes[i].style.top="1600px";
 		}
 	}
-	setTimeout(moveTrash,10);
+	setTimeout(moveBoxes,10);
 }
 
 function dragStart() 
@@ -73,13 +72,13 @@ function dragStart()
 	this.className += ' hold';
 	setTimeout(() => (this.className = 'invisible'), 0);
 	foo = this.id;
+	console.log("drag start");
 	audio.play();
 }
 
 function dragEnd() 
 {
 	this.active = false;
-	console.log('dragEnd');
 	this.className = 'fill';
 }
 
@@ -93,6 +92,7 @@ function dragEnter(e)
 {
   e.preventDefault();
   this.className += ' hovered';
+  console.log("dragEnter");
 }
 
 function dragLeave() 
@@ -106,7 +106,6 @@ function dragDrop(e)
 	pause = true;
 	console.log('dragDrop');
 	this.className = 'SSB';
-	console.log(foo);
 	modalHeader.innerHTML = foo;
 	modal.style.display = "block";
 	currElement = document.getElementById(foo);
@@ -116,12 +115,14 @@ function dragDrop(e)
 
 function closeModal(e)
 {
+	console.log(e.target);
 	answer=e.target.id;
 
-	if(answer==currElement.firstElementChild.className)
+	if(answer==currElement.nextElementSibling.className)
 	{
 		modal.style.display="none";
 		currElement.style.display="none";
+
 	}
 	else
 	{
@@ -146,4 +147,3 @@ function  checkWin()
 	if(flag)
 		location.replace('https://gleece0.wixsite.com/smartslugbin');
 }
-};
