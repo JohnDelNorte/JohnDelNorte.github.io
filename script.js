acb05323a81441b14f0ce88e2ba551a7508dd764
JohnDelNorte <jbbennet@ucsc.edu>
2020-05-14 13:55:56 -0700

    stack exchange ftw?

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 
+window.addEventListener("dragover",function(e){
+	e = e || event;
+	e.preventDefault();
+  },false);
+  window.addEventListener("drop",function(e){
+	e = e || event;
+	e.preventDefault();
+  },false);
+
 window.onload = function()
 {
 
